7f21a480982738f655993cfb1df7050d9803471b
pawelsawicz <pawel@sawicz.eu>
2013-11-17 22:53:53 +0100

    ExtendedSplashScreen added

---
+++ Peregrin/Peregrin/View/Common/ExtendedSplashScreen.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Peregrin.View.Common
+{
+    public partial class ExtendedSplashScreen : PhoneApplicationPage
+    {
+        public ExtendedSplashScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
