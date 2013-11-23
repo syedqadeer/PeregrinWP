b6bd5bbe615f0c1d4d144e50f7e8b45ba0e59588
pawelsawicz <pawel@sawicz.eu>
2013-11-23 17:59:48 +0100

    implement contextmenu (while holding button) with Pin to start menu action

--- Peregrin/Peregrin/View/Panorama/VehiclesView.xaml.cs
+++ Peregrin/Peregrin/View/Panorama/VehiclesView.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Peregrin.Resources;
 
 namespace Peregrin.View.Panorama
 {
@@ -15,6 +16,25 @@ namespace Peregrin.View.Panorama
         public VehiclesView()
         {
             InitializeComponent();
+        }
+
+        private void PinVehicle_Click(object sender, RoutedEventArgs e)
+        {
+            var shellTile = ShellTile.ActiveTiles.FirstOrDefault(x => x.NavigationUri.ToString().Contains("?id="));
+
+            var liveTile = new StandardTileData
+            {
+                Title = "126"               
+            };
+
+            if (shellTile != null)
+            {
+                MessageBox.Show(AppResources.TileIsAlreadyExist);
+            }
+            else
+            {
+                ShellTile.Create(new Uri("/View/Portrait/MapView.xaml", UriKind.Relative), liveTile);
+            }
         }
     }
 }
