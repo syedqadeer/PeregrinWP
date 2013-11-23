54793007affd3ee379cdf5bdc9cfc43ca4ed2009
pawelsawicz <pawel@sawicz.eu>
2013-11-23 17:58:46 +0100

    add extendedsplashscreen and navigation to mainview

--- Peregrin/Peregrin/View/Common/ExtendedSplashScreen.xaml.cs
+++ Peregrin/Peregrin/View/Common/ExtendedSplashScreen.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.Threading.Tasks;
 
 namespace Peregrin.View.Common
 {
@@ -15,6 +16,19 @@ namespace Peregrin.View.Common
         public ExtendedSplashScreen()
         {
             InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            NavigateToMenu();
+
+            base.OnNavigatedTo(e);
+        }
+
+        private async void NavigateToMenu()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            NavigationService.Navigate(new Uri("/View/Portrait/MainView.xaml", UriKind.Relative));
         }
     }
 }
