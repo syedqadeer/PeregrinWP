ed0c7787e6cb81c747fc208b2995068663a4685d
pawelsawicz <pawel@sawicz.eu>
2013-11-24 03:05:09 +0100

    add VehicleType enum

---
+++ Peregrin/Peregrin/Enums/VehicleType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peregrin.Enums
+{
+    public enum VehicleType
+    {
+        Bus,
+        Train,
+        Tram
+    }
+}
