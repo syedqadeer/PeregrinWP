5fbd48c4862ec7e27a441fd71adfb3d25eec1fa4
pawelsawicz <pawel@sawicz.eu>
2013-11-24 03:04:56 +0100

    add RestSharp extension that allow use await keyowrd, it return as Task, and Taks<T>

---
+++ Peregrin/Peregrin/Extensions/RestSharpExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using RestSharp;
+
+namespace Peregrin.Extensions
+{
+    public static class RestSharpExtension
+    {
+        private static Task<T> SelectAsync<T>(this IRestClient client, IRestRequest request,
+            Func<IRestResponse, T> selector)
+        {
+            var tsc = new TaskCompletionSource<T>();
+            var asyncHandler = client.ExecuteAsync(request, r =>
+            {
+                if (r.ErrorException == null)
+                {
+                    tsc.SetResult(selector(r));
+                }
+                else
+                {
+                    tsc.SetException(r.ErrorException);
+                }
+            });
+
+            return tsc.Task;
+        }
+
+        public static Task<string> GetContentAsync(this IRestClient client, IRestRequest request)
+        {
+            return client.SelectAsync(request, r => r.Content);
+        }
+
+        public static Task<IRestResponse> GetResponseAsync(this IRestClient client, IRestRequest request)
+        {
+            return client.SelectAsync(request, r => r);
+        }
+    }
+}
