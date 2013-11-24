e47d4fe9f2ab7070f38edf43ffe248288d8322bc
pawelsawicz <pawel@sawicz.eu>
2013-11-25 00:22:33 +0100

    create interface of VehicleProvider that allow us to get live vehicles informations

---
+++ Peregrin/Peregrin.Services/Providers/IVehicleProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Peregrin.Data.Interface;
+using Peregrin.Enums;
+
+namespace Peregrin.Services.Providers
+{
+    public interface IVehicleProvider
+    {
+        IVehicle GetVehicle(string name, VehicleType vehicleType);
+        IEnumerable<IVehicle> GetVehicles(IDictionary<string, VehicleType> dictionaryOfVehicles);
+    }
+}
