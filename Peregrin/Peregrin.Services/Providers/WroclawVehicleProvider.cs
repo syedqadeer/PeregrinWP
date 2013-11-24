49867913f5db180c30ff5b2c56065922bc353c52
pawelsawicz <pawel@sawicz.eu>
2013-11-25 00:44:48 +0100

    create implementation for IVehicleProvider for Wroclaw

---
+++ Peregrin/Peregrin.Services/Providers/WroclawVehicleProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Peregrin.Data.Interface;
+using Peregrin.Enums;
+
+namespace Peregrin.Services.Providers
+{
+    public class WroclawVehicleProvider : IVehicleProvider
+    {
+        private readonly string _apiClient = "http://pasazer.mpk.wroc.pl/position.php";
+
+        public IVehicle GetVehicle(string name, VehicleType vehicleType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IVehicle> GetVehicles(IDictionary<string, VehicleType> dictionaryOfVehicles)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
