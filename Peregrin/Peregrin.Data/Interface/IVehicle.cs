875efb597bdcc4492c93cfe5ce4fb2aa0a42517a
pawelsawicz <pawel@sawicz.eu>
2013-11-24 03:02:58 +0100

    add interface and class for vehicle

---
+++ Peregrin/Peregrin.Data/Interface/IVehicle.cs
@@ -0,0 +1,18 @@
+﻿using Peregrin.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peregrin.Data.Interface
+{
+    public interface IVehicle
+    {
+        string Name { get; set; }
+        VehicleType Type { get; set; }
+        double X { get; set; }
+        double Y { get; set;}
+
+    }
+}
---
+++ Peregrin/Peregrin.Data/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Peregrin.Data.Interface;
+using Peregrin.Enums;
+
+namespace Peregrin.Data
+{
+    public class Vehicle : IVehicle
+    {
+        public string Name { get; set; }
+        public VehicleType Type { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+
+
+    }
+}
