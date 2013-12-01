8beef7d13d3b3ea8fcaaa3bd70240bc9508d617f
pawelsawicz <pawel@sawicz.eu>
2013-12-01 05:50:29 +0100

    create bootstrap in main project

---
+++ Peregrin/Peregrin/Bootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Peregrin.Services.Automapper;
+
+namespace Peregrin
+{
+    public static class Bootstrapper
+    {
+        public static void MapperInitialize()
+        {
+            Mapper.Initialize(cfg => cfg.AddProfile<WroclawVehicleConfig>());
+        }
+    }
+}
