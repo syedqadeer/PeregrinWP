eba1a50a46319a5674ed853850e0482c7d3c9a7f
pawelsawicz <pawel@sawicz.eu>
2013-12-25 13:48:01 +0100

    Add City model, and SupportedCities ViewModel

---
+++ Peregrin/Peregrin.Data/City.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peregrin.Data
+{
+    public class City
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
