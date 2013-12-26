f87b19d3a3aba24b726fd1f3b455dc582d21e8b7
pawelsawicz <pawel@sawicz.eu>
2013-12-26 02:22:44 +0100

    Add autogenerate bus, tram and filtering by its name

--- Peregrin/Peregrin/View/Panorama/VehiclesView.xaml.cs
+++ Peregrin/Peregrin/View/Panorama/VehiclesView.xaml.cs
@@ -5,18 +5,32 @@ using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using Peregrin.Resources;
 using System.Windows.Controls;
+using Peregrin.Services.Providers;
+using Peregrin.Common.Enum;
+using System.Collections.Generic;
+using Microsoft.Phone.Reactive;
 
 namespace Peregrin.View.Panorama
 {
     public partial class VehiclesView : PhoneApplicationPage
     {
+        private readonly WroclawVehicleProvider _provider;
+        private readonly IObservable<string> _buses;
+        private readonly IObservable<string> _trams;
+
         public VehiclesView()
-        {
+        { 
             InitializeComponent();
+            _provider = new WroclawVehicleProvider();
+            _buses = _provider.GetAvailableVehicles(VehicleType.Bus).ToObservable();
+            _trams = _provider.GetAvailableVehicles(VehicleType.Tram).ToObservable();
+            BusListBox.ItemsSource = _buses.ToEnumerable();
+            TramListBox.ItemsSource = _trams.ToEnumerable();
         }
 
         private void PinVehicle_Click(object sender, RoutedEventArgs e)
         {
+            
             var shellTile = ShellTile.ActiveTiles.FirstOrDefault(x => x.NavigationUri.ToString().Contains("?id="));
 
             var liveTile = new StandardTileData
@@ -40,8 +54,21 @@ namespace Peregrin.View.Panorama
 
             if (filterContext.Text != null)
             {
-
+                var name = filterContext.Text;
+                BusListBox.ItemsSource = _buses.Where(x=>x.Contains(name)).ToEnumerable();
             }
+        }
+
+        private void TramTextBoxFilter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+             var filterContext = sender as TextBox;
+
+             if (filterContext.Text != null)
+             {
+                 var name = filterContext.Text;
+                 TramListBox.ItemsSource = _trams.Where(x => x.Contains(name)).ToEnumerable();
+
+             }
         }
     }
 }
